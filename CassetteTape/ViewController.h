e343b28d78bd44de83b0e8229110abef42e31a03
Kevin Johnson <kevin@dialexa.com>
2012-02-22 16:08:31 -0600

    Initial Commit

---
+++ CassetteTape/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CassetteTape
+//
+//  Created by Kevin Johnson on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
